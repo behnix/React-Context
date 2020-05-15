ec54cc484f467d9027157170625a484b8eb8638c
Behnam Mahmoodi <behnam.itc@outlook.com>
2020-05-16 00:28:43 +0430

    Improve project eith useContext hook

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,21 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isLight, Light, Dark } = this.context;
-    const theme = isLight ? Light : Dark;
-    return (
-      <div
-        className="book-list"
-        style={{ color: theme.syntax, background: theme.ui }}
-      >
-        <ul>
-          <li style={{ background: theme.bg }}>C# for beginner</li>
-          <li style={{ background: theme.bg }}>React & Redux self study</li>
-          <li style={{ background: theme.bg }}>Javascript from scratch</li>
-        </ul>
-      </div>
-    );
-  }
+const BookList = () => {
+  const {isLight, Light, Dark} = useContext(ThemeContext)
+  const theme = isLight ? Light : Dark;
+  return ( 
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.ui }}
+    >
+      <ul>
+        <li style={{ background: theme.bg }}>C# for beginner</li>
+        <li style={{ background: theme.bg }}>React & Redux self study</li>
+        <li style={{ background: theme.bg }}>Javascript from scratch</li>
+      </ul>
+    </div>
+   );
 }
-
+ 
 export default BookList;
